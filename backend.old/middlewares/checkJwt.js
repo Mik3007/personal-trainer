3eb887034eec6381ba168f2455fb43f5ee1fc0ae
Michele Altieri <156221751+Mik3007@users.noreply.github.com>
2024-09-01 11:01:53 +0200

    Delete backend.old directory

--- backend.old/middlewares/checkJwt.js
+++
@@ -1,9 +0,0 @@
-import { auth } from 'express-oauth2-jwt-bearer';
-
-const checkJwt = auth({
-  audience: 'https://dev-m84k5j50syqrc2d7.us.auth0.com/api/v2/',
-  issuerBaseURL: 'https://dev-m84k5j50syqrc2d7.us.auth0.com/',
-  tokenSigningAlg: 'RS256'
-});
-
-export default checkJwt;
