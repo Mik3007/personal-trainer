a7cc285b1eef6e84af7cc8fbe21ccb2a2aedf231
Mik3007 <michelealtieri3007@gmail.com>
2024-09-06 19:06:04 +0200

    configurazione per il deploy

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", 
+            "https://francescorauccipt-5uozg2jer-micheles-projects-d3df038b.vercel.app"],
+    credentials: true,
   })
 );
 
