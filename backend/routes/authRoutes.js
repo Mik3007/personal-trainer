cd4be6bc99571345f2970dd68cd183b648c35764
Mik3007 <michelealtieri3007@gmail.com>
2024-09-06 19:31:35 +0200

    modifiche

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -46,35 +46,6 @@ router.post('/register', async (req, res) => {
 });
 
 // Login
-router.post('/login', loginUser), async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-
-    if (user && (await user.matchPassword(password))) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
-      });
-      const isAdmin = ['michelealtieri3007@gmail.com'].includes(user.email);
-
-      res.json({
-        _id: user._id,
-        email: user.email,
-        nome: user.nome,
-        cognome: user.cognome,
-        email: req.body.email,
-        dataNascita: req.body.dataNascita,
-        isAdmin: isAdmin,
-        token
-      });
-    } else {
-      res.status(401).json({ message: 'Credenziali non valide' });
-    }
-  } catch (error) {
-    console.error('Errore durante il login:', error);
-    res.status(500).json({ message: 'Errore durante il login' });
-  }
-};
+router.post('/login', loginUser);
 
 export default router;
