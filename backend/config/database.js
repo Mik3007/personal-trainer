95ba4e1e80a67ea42d3d3f40e0dce42a7dc64731
Michele Altieri <156221751+Mik3007@users.noreply.github.com>
2024-09-11 13:34:10 +0200

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 
+// Funzione per connettere il database MongoDB
 const connectDB = async () => {
   try {
+    // Tenta di stabilire una connessione al database usando l'URI fornito nelle variabili d'ambiente
     await mongoose.connect(process.env.MONGO_URI)
     console.log('MongoDB connesso');
   } catch (err) {
+    // Se si verifica un errore durante la connessione, lo registra e termina il processo
     console.error('Errore di connessione MongoDB:', err);
     process.exit(1);
   }
